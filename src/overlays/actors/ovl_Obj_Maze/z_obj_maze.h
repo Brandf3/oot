13b91b1fd05d40d292eefb2e71f9cce4be2375e1
Brandon Francis <brandon55604@gmail.com>
2024-10-17 20:44:41 -0700

    Origin shift

--- src/overlays/actors/ovl_Obj_Maze/z_obj_maze.h
+++ src/overlays/actors/ovl_Obj_Maze/z_obj_maze.h
@@ -4,13 +4,20 @@
 #include "ultra64.h"
 #include "global.h"
 
+typedef struct {
+    u8 type;           // Cell type (e.g., MAZE_UP, MAZE_DOWN, etc.)
+    s8 rightWallIdx;   // Index in the wallActors array for the right wall (-1 if no right wall)
+    s8 topWallIdx;     // Index in the wallActors array for the top wall (-1 if no top wall)
+} Cell;
+
 struct ObjMaze;
 
 typedef void (*ObjMazeActionFunc)(struct ObjMaze*, PlayState*);
 
 typedef struct ObjMaze {
     Actor actor;
-    u8 maze[10][10];
+    Cell maze[10][10];
+    Actor* wallActors[101];
     unsigned long int next;
     u8 start;
     u8 originShiftPoint;
