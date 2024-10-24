f8be462d0d0fce7e845eb7779ef4bd5e351c68de
Brandon Francis <brandon55604@gmail.com>
2024-10-23 23:16:20 -0700

    KeIngo

--- mod_assets/scenes/my_scenes/test_map/test_map_room_0.c
+++ mod_assets/scenes/my_scenes/test_map/test_map_room_0.c
@@ -26,17 +26,18 @@ SceneCmd test_map_room_0_header00[] = {
 s16 test_map_room_0_header00_objectList[LENGTH_TEST_MAP_ROOM_0_HEADER00_OBJECTLIST] = {
     OBJECT_MAZE,
 	OBJECT_RD,
-	OBJECT_DH
+	OBJECT_DH,
+	OBJECT_FIREFLY
 };
 
 ActorEntry test_map_room_0_header00_actorList[LENGTH_TEST_MAP_ROOM_0_HEADER00_ACTORLIST] = {
     // Custom Actor
-    {
-        /* Actor ID   */ ACTOR_OBJ_MAZE,
-        /* Position   */ { 0, 0, 0 },
-        /* Rotation   */ { DEG_TO_BINANG(0.000), DEG_TO_BINANG(0.000), DEG_TO_BINANG(0.000) },
-        /* Parameters */ 0x0000
-    },
+    // {
+    //     /* Actor ID   */ ACTOR_OBJ_MAZE,
+    //     /* Position   */ { 0, 0, 0 },
+    //     /* Rotation   */ { DEG_TO_BINANG(0.000), DEG_TO_BINANG(0.000), DEG_TO_BINANG(0.000) },
+    //     /* Parameters */ 0x0000
+    // },
 	// {
     //     /* Actor ID   */ ACTOR_EN_RD,
     //     /* Position   */ { 50, 0, 0 },
@@ -49,6 +50,12 @@ ActorEntry test_map_room_0_header00_actorList[LENGTH_TEST_MAP_ROOM_0_HEADER00_AC
     //     /* Rotation   */ { DEG_TO_BINANG(0.000), DEG_TO_BINANG(0.000), DEG_TO_BINANG(0.000) },
     //     /* Parameters */ 0x0002
     // },
+	{
+        /* Actor ID   */ ACTOR_EN_FIREFLY,
+        /* Position   */ { 50, 0, 0 },
+        /* Rotation   */ { DEG_TO_BINANG(0.000), DEG_TO_BINANG(0.000), DEG_TO_BINANG(0.000) },
+        /* Parameters */ 0x0002
+    },
 };
 
 RoomShapeNormal test_map_room_0_shapeHeader = {
